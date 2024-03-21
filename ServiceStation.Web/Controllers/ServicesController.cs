72b26ff64f7f5d6dd1c92a9ffac4e308883a62cd
Charuka Kavinda <ckavinda57@gmail.com>
2024-03-21 18:27:18 +0530

    Add project files.

---
+++ ServiceStation.Web/Controllers/ServicesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceStation.Web.Models;
+using System.Xml.Serialization;
+
+namespace ServiceStation.Web.Controllers
+{
+    public class ServicesController : Controller
+    {
+        [ViewData]
+        public string PageTitle { get; set; }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            PageTitle = "Service Details";
+            var service = new VehicleService
+            {
+                Id = 1,
+                Title = "Test",
+                Description = "Test",
+                Coast = 23.99m,
+                IsDeleted = true
+            };
+
+           // ViewBag.Service = service;
+            return View(service);
+
+        }
+         
+    }
+}

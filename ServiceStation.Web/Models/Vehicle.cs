a4acaf99da2c51965ad5039e7a288e961e5e48f0
Charuka Kavinda <ckavinda57@gmail.com>
2024-03-22 20:23:34 +0530

    Finishing Models In Project

---
+++ ServiceStation.Web/Models/Vehicle.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Routing.Constraints;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace ServiceStation.Web.Models
+{
+    public class Vehicle
+    {
+       public int Id { get; set; }  
+       public string Make { get; set; }
+       public string Model { get; set; } 
+       public int Year { get; set; }
+       public string  Color { get; set; }
+       public bool IsDeleted { get; set; }
+       
+    }
+}
